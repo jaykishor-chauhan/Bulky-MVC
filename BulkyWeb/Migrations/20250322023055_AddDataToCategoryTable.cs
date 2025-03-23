caf8ae36a32c151da8ee94a0e926997b97872914
Jaykishor Chauhan <jaykishor04.3.7.np@gmail.com>
2025-03-23 09:02:15 +0530

    updated

---
+++ BulkyWeb/Migrations/20250322023055_AddDataToCategoryTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataToCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "ID", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 5, "Jaykishor" },
+                    { 2, 3, "Rishu" },
+                    { 3, 2, "Sahil" },
+                    { 4, 7, "Shiv" },
+                    { 5, 1, "Krishana" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: 5);
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key] 
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [Range(1,100, ErrorMessage = "Enter Valid Order must be between 1 and 100.")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
